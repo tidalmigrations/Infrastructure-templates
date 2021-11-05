063aba1168d308868985aa7d4626af20ae63ad71
mario mejia <jmariomejiap@gmail.com>
2021-11-05 11:10:57 -0300

    include terraform and script

---
+++ ubuntu/ubuntu-20-04/instances/terraform.tfvars
@@ -0,0 +1,2 @@
+region = "us-west-2"
+ami = "ami-09f504dbc7d4dd3a0"
---
+++ ubuntu/ubuntu-20-04/scripts/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -x
+
+echo ++ Updating ++
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+
+echo ++ Installing Tidal Tools ++
+curl https://get.tidal.sh/unix | bash
+
+echo ++ Installing PIP ++
+sudo apt-get install --yes python3-pip
+
+echo ++ Updating PATH ++
+export PATH=/home/ubuntu/.local/bin:$PATH
+
+echo ++ Upgrading PIP ++
+python3 -m pip install --upgrade pip
+
+echo ++ Installing jq ++
+sudo apt-get install --yes jq
+
+echo ++ Installing Machine-Stats ++
+python3 -m pip install machine-stats
+
+echo ++ Installing Nmap ++
+sudo apt-get install --yes  nmap
+
+echo ++++ DONE ++++
