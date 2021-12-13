a22589829e6e22a12bc65b1fd7324361c03baa7f
Pranav Malaviya <pranavmalaviya2@gmail.com>
2021-12-13 21:30:34 +1100

    Add docker installation step

--- aws/rhel/rhel7/scripts/setup.sh
+++ aws/rhel/rhel7/scripts/setup.sh
@@ -24,4 +24,20 @@ curl https://get.tidal.sh/unix | bash
 echo ++ Installing Nmap
 sudo yum install -y nmap
 
+echo ++ Installing Docker
+
+## Install required CentOS packages
+sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/fuse3-libs-3.6.1-4.el7.x86_64.rpm
+sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/fuse3-devel-3.6.1-4.el7.x86_64.rpm
+sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/fuse-overlayfs-0.7.2-6.el7_8.x86_64.rpm
+sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.119.2-1.911c772.el7_8.noarch.rpm
+sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/slirp4netns-0.4.3-4.el7_8.x86_64.rpm
+
+# Add docker repo and install docker engine and containerd
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+docker --version
+
 echo ++++ DONE ++++
