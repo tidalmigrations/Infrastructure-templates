a710043a96369ab8c294e882e12c4076849ca4d2
Pranav Malaviya <pranavmalaviya2@gmail.com>
2021-12-14 20:06:35 +1100

    Use rhel-extras instead of individually installing dependencies

--- aws/rhel/rhel7/scripts/setup.sh
+++ aws/rhel/rhel7/scripts/setup.sh
@@ -26,18 +26,22 @@ sudo yum install -y nmap
 
 echo ++ Installing Docker
 
-## Install required CentOS packages
-sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/fuse3-libs-3.6.1-4.el7.x86_64.rpm
-sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/fuse3-devel-3.6.1-4.el7.x86_64.rpm
-sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/fuse-overlayfs-0.7.2-6.el7_8.x86_64.rpm
-sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.119.2-1.911c772.el7_8.noarch.rpm
-sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/slirp4netns-0.4.3-4.el7_8.x86_64.rpm
-
-# Add docker repo and install docker engine and containerd
+# Install yum-utils
 sudo yum install -y yum-utils
+
+# Enable extras repo
+sudo yum-config-manager --enable rhel-7-server-rhui-extras-rpms
+
+# Check that repo was enabled
+sudo yum repolist enabled
+
+# Add Docker repo
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker engine and containerd
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
+# Verify that Docker was installed
 docker --version
 
 echo ++++ DONE ++++
