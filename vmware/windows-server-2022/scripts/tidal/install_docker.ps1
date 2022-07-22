7a3cb24f8531f5acb68a218bc4e008cb7f69ee05
Pranav Malaviya <pranavmalaviya2@gmail.com>
2022-07-22 19:45:00 +1000

    Install NuGet provider before docker

--- vmware/windows-server-2022/scripts/tidal/install_docker.ps1
+++ vmware/windows-server-2022/scripts/tidal/install_docker.ps1
@@ -1,3 +1,6 @@
+# Install NuGet provider
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope 'AllUsers' -Force
+
 # Install docker module
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Confirm:$False -Force
 
