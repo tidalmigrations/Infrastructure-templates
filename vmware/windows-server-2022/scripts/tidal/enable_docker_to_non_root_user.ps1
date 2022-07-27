a406651bab0f27c7fa29a3cc6bd3e1f12395810e
Pranav Malaviya <pranavmalaviya2@gmail.com>
2022-07-27 19:17:50 +1000

    Revert changes of adding vagrant user to docker groups

--- vmware/windows-server-2022/scripts/tidal/enable_docker_to_non_root_user.ps1
+++
@@ -1,7 +0,0 @@
-# Install dockeraccesshelper module to allow
-# docker access to non-root user
-# Source: https://github.com/tfenster/dockeraccesshelper
-Install-Module -Name dockeraccesshelper -Force
-
-# Add vagrant user for docker access
-Add-AccountToDockerAccess "\vagrant"
