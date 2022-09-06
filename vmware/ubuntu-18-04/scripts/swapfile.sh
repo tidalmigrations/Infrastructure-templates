a8126c2596f491a7ebdf5061d2783759f9e8691b
Pranav Malaviya <pranavmalaviya2@gmail.com>
2022-09-06 16:14:52 +1000

    Add swapfile

---
+++ vmware/ubuntu-18-04/scripts/swapfile.sh
@@ -0,0 +1,13 @@
+# Disable existing swap
+sudo swapoff -a
+
+# Create a swap of 2GB
+sudo fallocate -l 2G /swapfile
+
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo sed -i '$a/swapfile swap swap defaults 0 0' /etc/fstab
+
+# Show free memory
+sudo free -h
